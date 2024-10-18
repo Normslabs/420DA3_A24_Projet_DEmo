41e2ec022a20729b6b0329390048ac515047389c
Marc-Eric Boury <marc-eric.boury@collegelasalle.com>
2024-10-18 10:41:06 -0400

    niaiserie

--- Exemple_Enseignant/Program.cs
+++ Exemple_Enseignant/Program.cs
@@ -1,15 +1,11 @@
 ﻿using _420DA3_A24_Exemple_Enseignant.Business;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _420DA3_A24_Exemple_Enseignant;
 internal static class Program {
 
     [STAThread]
     public static void Main(string[] args) {
+        // hello coco
         new ExempleApplication().Start();
     }
 }
